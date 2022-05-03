fd03d8ab531b6c1c584233fe6fc7d235f1e471a4
David Luu <dluu.audeo@gmail.com>
2022-05-03 10:03:31 -0700

    updated server dir and created pkg

--- client/src/components/coffeeshops/Coffeeshops.js
+++ client/src/components/coffeeshops/Coffeeshops.js
@@ -10,18 +10,27 @@ class Coffeeshops extends Component {
   }
 
   componentDidMount() {
-    fetch('/api/coffeeshops')
-    .then(res => res.json())
-    .then(coffeeshops => this.setState({ coffeeshops }, () => console.log('Coffeeshops fetched...', coffeeshops)));
+    fetch('https://api.yelp.com/v3/businesses/search?latitude=32.8073216&longitude=-117.1554304&term=coffee'), {
+      method: 'GET',
+      headers: { 
+        Authorization: `Bearer QzupW1zdd0Vd-l-Veb-8lnMKkVt4RYrf6A865pPzlRLtV3NCOvxYvfLc1L54gMrHQQdy43zM1go_S55R2MXJmlvQf-NWlevKHbYzIVMKwlPS7y9_PyVJ09Mk80hgYnYx`,
+      }
+    .then(function (res) {
+      console.log(res);
+      return res.json();
+    })
+    // .then(function (coffeeshops) {
+    //   this.setState({ coffeeshops }, () => console.log('Coffeeshops fetched...', coffeeshops));
+    // })
   }
-
+}
   render() {
     return (
       <div className="App">
         <h2>Coffeeshops</h2>
-        <ul>
+        {/* <ul>
           {this.state.coffeeshops.map(coffeeshop => <li key={coffeeshop.id}>{coffeeshop.name}</li>)}
-        </ul>
+        </ul> */}
       </div>
     );
   }
