5ba4f83f03ad0080072258be74abb519cd2543b4
David Luu <dluu.audeo@gmail.com>
2022-05-11 10:19:02 -0700

    updated env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+// const fetch = require('node-fetch');
+// const yelp_API= "https://api.yelp.com/v3/businesses/search?latitude=32.8073216&longitude=-117.1554304&term=coffee";
+
 
 app.get('/api/coffeeshops', (req, res) => {
     const coffeeshops = [
@@ -7,10 +10,20 @@ app.get('/api/coffeeshops', (req, res) => {
         {id:2, name: 'Better Buzz', address: '456 Main St.', city: 'New York', state: 'NY', zip: '12345'},
         {id:3, name: 'Dutch Bros', address: '789 Main St.', city: 'New York', state: 'NY', zip: '12345'},
     ];
-
-    res.json(coffeeshops);
+    return res.json(coffeeshops);
 }); 
 
+// function getCoffeeshops() {
+//     fetch(yelp_API, {
+//         method: 'GET',
+//         headers: { 
+//             Authorization: `Bearer `PROCESS.ENV.API_KEY`,
+//         }
+//     }).then(function (res) {
+//         console.log(res);
+//         return res.json();
+//     })
+// }
 
 const port = 5000;
 
